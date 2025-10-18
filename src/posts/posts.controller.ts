12e33670d8654f519012259d8465cb701ec0c2d1
Sudeep Gautam <sudeepgautam777@gmail.com>
2025-10-18 17:31:23 +0545

    Creating Post method

--- src/posts/posts.controller.ts
+++ src/posts/posts.controller.ts
@@ -1,4 +1,19 @@
-import { Controller } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Post,
+  UsePipes,
+  ValidationPipe,
+} from '@nestjs/common';
+import { CreatePostDto } from './dtos/CreatePost.dto';
+import { PostsService } from './posts.service';
 
 @Controller('posts')
-export class PostsController {}
+export class PostsController {
+  constructor(private postService: PostsService) {}
+  @Post()
+  @UsePipes(ValidationPipe)
+  createPost(@Body() { userId, ...createPostData }: CreatePostDto) {
+    return this.postService.createPost(userId, createPostData);
+  }
+}
--- src/posts/posts.service.ts
+++ src/posts/posts.service.ts
@@ -1,4 +1,16 @@
 import { Injectable } from '@nestjs/common';
+import { Prisma } from '@prisma/client';
+import { PrismaService } from 'src/prisma/prisma.service';
 
 @Injectable()
-export class PostsService {}
+export class PostsService {
+  constructor(private prisma: PrismaService) {}
+  createPost(userId: number, data: Prisma.PostCreateWithoutUserInput) {
+    return this.prisma.post.create({
+      data: {
+        ...data,
+        userId,
+      },
+    });
+  }
+}
