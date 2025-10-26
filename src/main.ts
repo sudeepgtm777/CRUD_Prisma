af50b592182a2bc50b578ee14bbbae981887ebea
Sudeep Gautam <sudeepgautam777@gmail.com>
2025-10-26 15:37:34 +0545

    Setting up Swagger for documentation

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,20 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder()
+    .setTitle('Posts API')
+    .setDescription('API documentation for Posts and GroupPosts')
+    .setVersion('1.0')
+    .addTag('posts')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
